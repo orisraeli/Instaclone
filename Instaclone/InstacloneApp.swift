a77d23bf786984dc25552cda979b0f518d20a5a3
Or Israeli <isra.or@gmail.com>
2023-08-04 16:13:38 +0300

    Initial Commit

---
+++ Instaclone/InstacloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstacloneApp.swift
+//  Instaclone
+//
+//  Created by Or Israeli on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct InstacloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
