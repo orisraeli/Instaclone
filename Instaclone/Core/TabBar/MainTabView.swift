4d6ab7ef3e12fabeb7002add83fb901147d3c535
Or Israeli <isra.or@gmail.com>
2023-08-04 16:24:52 +0300

    add TabView and project folders

---
+++ Instaclone/Core/TabBar/MainTabView.swift
@@ -0,0 +1,46 @@
+//
+//  MainTabView.swift
+//  Instaclone
+//
+//  Created by Or Israeli on 04/08/2023.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+		TabView {
+			Text("Feed")
+				.tabItem {
+					Image(systemName: "house")
+				}
+			
+			Text("Search")
+				.tabItem {
+					Image(systemName: "magnifyingglass")
+				}
+			
+			Text("Upload")
+				.tabItem {
+					Image(systemName: "plus.square")
+				}
+			
+			Text("Notifications")
+				.tabItem {
+					Image(systemName: "heart")
+				}
+			
+			Text("Profile")
+				.tabItem {
+					Image(systemName: "person")
+				}
+		}
+		.tint(.black)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
