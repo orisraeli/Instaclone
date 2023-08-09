c8c50a57893c32105337e8b5ce755e58b3fb6d33
Or Nathan Israeli <orisraeli@users.noreply.github.com>
2023-08-09 17:48:34 +0300

    import Firebase package

--- Instaclone/App/InstacloneApp.swift
+++ Instaclone/App/InstacloneApp.swift
@@ -5,10 +5,23 @@
 //  Created by Or Israeli on 04/08/2023.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+	func application(_ application: UIApplication,
+					 didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		FirebaseApp.configure()
+		
+		return true
+	}
+}
 
 @main
 struct InstacloneApp: App {
+	// register app delegate for Firebase setup
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
