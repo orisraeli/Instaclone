b316c0f8e4331b0824b0a09a1c950e94349ec21f
Or Israeli <isra.or@gmail.com>
2023-08-13 18:14:19 +0300

    implement changing user profile picture function

--- Instaclone/Core/Profile/ViewModel/EditProfileViewModel.swift
+++ Instaclone/Core/Profile/ViewModel/EditProfileViewModel.swift
@@ -24,6 +24,8 @@ class EditProfileViewModel: ObservableObject {
 		}
 	}
 	
+	private var uiImage: UIImage?
+	
 	init(user: User) {
 		self.user = user
 	}
@@ -31,25 +33,30 @@ class EditProfileViewModel: ObservableObject {
 	func loadImage(from item: PhotosPickerItem?) async {
 		guard let item = item else { return }
 		guard let data = try? await item.loadTransferable(type: Data.self) else { return }
+		guard let uiImage = UIImage(data: data) else { return }
 		
-		if let uiImage = UIImage(data: data) {
-			profileImage = Image(uiImage: uiImage)
-		}
+		self.uiImage = uiImage
+		self.profileImage = Image(uiImage: uiImage)
 	}
 	
 	func updateUserData() async throws {
 		var data = [String: Any]()
 		
 		//update profile image if changed
+		if let uiImage = uiImage {
+			print("Updating user's profile picture.")
+			let imageURL = try? await ImageUploader.uploadImage(uiImage)
+			data["profileImageURL"] = imageURL
+		}
 		
 		//update name & bio if changed
 		if !fullName.isEmpty && user.fullName != fullName {
-			print("Updating user full name: \(fullName)")
+			print("Updating user's full name: \(fullName)")
 			data["fullName"] = fullName
 		}
 		
 		if !bio.isEmpty && user.bio != bio {
-			print("Updating user bio: \(bio)")
+			print("Updating user's bio: \(bio)")
 			data["bio"] = bio
 		}
 		
